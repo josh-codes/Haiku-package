acdd6f104b7806d02247571b5ade800fa39ff76c
Josh <43808099+josh-codes@users.noreply.github.com>
2020-07-22 17:32:44 +0800

    Fix error

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Josh-codes", # Replace with your own username
+    name="haiku-Josh-codes", # Replace with your own username
     version="0.1",
     author="Josh",
     author_email="joshua.brest99@gmail.com",
