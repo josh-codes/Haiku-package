c0555d63d2d6183c00e0ec44c0f66982105339e2
Josh <43808099+josh-codes@users.noreply.github.com>
2020-08-13 21:38:00 +0800

    fix

--- haiku/UDPget.py
+++ haiku/UDPget.py
@@ -1,26 +1,48 @@
 import asyncio
-msg=""
-ip = ""
 class udpProto:
-    def connection_made(self, transport):
-        self.transport = transport
+	def __init__(self, on_done, msg, ip):
+		self.msg = msg
+		self.ip = ip
+		self.on_done = on_done
+		self.transport = None
+		self.msg.set_result("")
+		self.ip.set_result("")
+		self.on_done.set_result(False)
 
-    def datagram_received(self, data, addr):
-        msg = data.decode()
-        ip = addr
-        transport.close()
+	def connection_made(self, transport):
+		self.transport = transport
 
+	def datagram_received(self, data, addr):
+		transport.close()
+		self.msg.set_result(data.decode())
+		self.ip.set_result(addr)
+		self.on_done.set_result(True)
+
+	def connection_lost(self, stuff):
+		# I have to do something here
+		something = True
 
 async def get(timeout, bindip, revport):
-    loop = asyncio.get_running_loop()
-    transport, protocol = await loop.create_datagram_endpoint(
-        lambda: udpProto(),
-        local_addr = (bindip, revport))
+	loop = asyncio.get_running_loop()
+	on_done = loop.create_future()
+	msg = loop.create_future()
+	ip = loop.create_future()
+	time = timeout/10
+	transport, protocol = await loop.create_datagram_endpoint(
+		lambda: udpProto(on_done, msg, ip),
+		local_addr = (bindip, revport))
 
-    try:
-        await asyncio.sleep(timeout)
-    finally:
-    	if addr == "": 
-        	transport.close()
-        	ret = False
-        return [msg,ip]
+	try:
+		for i in range(10):
+			ondoneres = str(on_done).split('=')
+			ondoneres = (ondoneres[1]).split('>')
+			ondoneres = ondoneres[0]
+			if ondoneres == 'False':
+				await asyncio.sleep(time)
+	finally:
+		if str(ip) == "<Future finished result=\'\'>": 
+			transport.close()
+			ret = False
+		else:
+			ret = [msg,ip]
+	return ret
