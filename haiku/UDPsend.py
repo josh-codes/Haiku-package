2c65cc698c6f1ef6fb6d721337a0d8dd2c512f0b
Josh <43808099+josh-codes@users.noreply.github.com>
2020-08-13 17:09:33 +0800

    Async sockets

--- haiku/UDPsend.py
+++ haiku/UDPsend.py
@@ -1,29 +1,28 @@
-import socket, asyncoro
-import concurrent.futures
-async def send_send(broadcast, destip, bindip, eport, lport, data):
-	"""Send UDP packets"""
-	# Create socket
-	try:
-		s = asyncoro.AsynCoroSocket(socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0))
-	except:
-		return False
-	if broadcast:
-		# If target is a broadcast ip
-		s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-	# Define variables
-	lconfig = (bindip, lport)
-	econfig = (destip, eport)
-	sdata = data.encode("utf-8")
-	# Bind local port
-	s.bind(lconfig)
-	# Send data
-	s.sendto(sdata, econfig)
-	# Close socket
-	s.close()
-	return True
+import asyncio
+sdata=""
+addr=""
+class udpProto:
+	def connection_made(self, transport):
+		self.transport = transport
+		senddata = sdata.encode('utf8')
+		self.transport.sendto(senddata, addr)
+		transport.close()
+
+	def datagram_received(self, data, addr):
+		# I have to do something here
+		something = True
 
 async def send(broadcast, destip, bindip, eport, lport, data):
-	with concurrent.futures.ThreadPoolExecutor() as executor:
-	    future = executor.submit(await send_send, (broadcast, destip, bindip, eport, lport, data))
-	    return_value = future.result()
-	    print(return_value)
+	loop = asyncio.get_running_loop()
+	global sdata
+	sdata = data
+	global addr
+	addr = (destip, eport)
+	transport, protocol = await loop.create_datagram_endpoint(
+		lambda: udpProto(),
+		local_addr=(bindip, lport))
+
+	try:
+		await asyncio.sleep(6)
+	finally:
+		return True
